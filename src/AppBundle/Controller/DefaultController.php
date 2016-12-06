44dc7507e72e677daed398ebc3cac908d9ac060f
kaemic <kaemic@gmail.com>
2016-12-06 21:25:23 +0100

    add janusz, getpearsonswithjanuszes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Janusz;
+use AppBundle\Entity\Pearson;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -25,6 +28,7 @@ class DefaultController extends Controller
         foreach ($pearsons as $pkey => $pearson) {
             $ret[$pkey]['name'] = $pearson->getName();
             $ret[$pkey]['surname'] = $pearson->getSurname();
+            $ret[$pkey]['unit'] = 'Unit 3';
             $ret[$pkey]['janusze']['count'] = 0;
             foreach ($januszes as $janusz) {
                 if ($janusz->getPearson() == $pearson) {
@@ -33,6 +37,7 @@ class DefaultController extends Controller
                 }
             }
         }
+
         // replace this example code with whatever you need
         return $this->render('AppBundle:common:index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
@@ -41,11 +46,54 @@ class DefaultController extends Controller
     }
 
     /**
+     * @Route("/getpearsons", name="getpearsons")
+     */
+    public function getPearsonsWithJanuszesAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $pearsonRepository = $em->getRepository('AppBundle:Pearson');
+        $pearsons = $pearsonRepository->findAll();
+
+        $januszRepository = $em->getRepository('AppBundle:Janusz');
+        $januszes = $januszRepository->findAll();
+
+        $ret = [];
+        foreach ($pearsons as $pkey => $pearson) {
+            $ret[$pkey]['name'] = $pearson->getName();
+            $ret[$pkey]['surname'] = $pearson->getSurname();
+            $ret[$pkey]['unit'] = 'Unit 3';
+            $ret[$pkey]['janusze']['count'] = 0;
+            foreach ($januszes as $janusz) {
+                if ($janusz->getPearson() == $pearson) {
+                    $ret[$pkey]['janusze']['count']++;
+                    $ret[$pkey]['janusze']['items'][] = ['reason' => $janusz->getReason(), 'date' => $janusz->getDate()];
+                }
+            }
+        }
+
+        return new JsonResponse($ret);
+    }
+
+    /**
      * @Route("/addjanusz", name="addjanusz")
      */
     public function addJanuszAction(Request $request)
     {
-
+        if (null != $request->get('janusz_opis', null) && null != $request->get('janusz_pearson', null)) {
+            $em = $this->getDoctrine()->getManager();
+            $pearsonRepository = $em->getRepository('AppBundle:Pearson');
+            $Pearson = $pearsonRepository->findOneById($request->get('janusz_pearson'));
+            $Janusz = new Janusz();
+            $Janusz->setReason($request->get('janusz_opis'));
+            $Janusz->setPearson($Pearson);
+            $Janusz->setDate(new \DateTime());
+            $em->persist($Janusz);
+            $em->flush();
+            return new JsonResponse('Janusz added for ' . $Pearson->getName() . ' ' . $Pearson->getSurname());
+        } else{
+            return new JsonResponse('error, empty janusz or pearson');
+        }
     }
 
     /**
